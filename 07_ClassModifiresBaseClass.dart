a63466784699e3dea349b32a7f52b42eee3425bb
AkshayChauhan3 <akshaychauhanonearth@gmail.com>
2025-08-07 16:24:10 +0000

    base class done

--- 07_ClassModifiresBaseClass.dart
+++ 07_ClassModifiresBaseClass.dart
@@ -1,19 +1,28 @@
 // base class : can be extandable only if new class is base, final, sealed, or interface.
 
 void main() {
-
+    father f = new father();
+    f.printFather();
 }
 
 //created a base class
-base class father() {
-    void printFather {
+base class father {
+    void printFather() {
         print("father");
     }
 }
 
-//now if we want to extend it 
+// now if we want to extend it => it will give error beacuse this is not base, final, sealed, or interface
 
-class son extends father {
+// class son extends father {
+//     void printFather() {
+//         print("papa");
+//     }
+// }
+
+// but if we make it base class then it will work
+
+base class son extends father {
     void printFather() {
         print("papa");
     }
